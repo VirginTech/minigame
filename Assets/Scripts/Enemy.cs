e3bbc5bfb2e476e774aeda037f685fc2dbd75a80
OOTANI,Kenji <ootani@virgintech.co.jp>
2016-03-30 15:25:08 +0900

    Enemyの移動、消滅

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Token {
+
+	// Use this for initialization
+	void Start () {
+		SetSize (SpriteWidth/2,SpriteHeight/2);
+		float dir = Random.Range (0, 359);
+		float spd = 2;
+		SetVelocity (dir,spd);
+		//Debug.Log ("角度：" + dir);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 min = GetWorldMin ();
+		Vector2 max = GetWorldMax ();
+		if(X<min.x || max.x<X){
+			VX *= -1;
+			ClampScreen ();
+		}
+		if(Y<min.y || max.y<Y){
+			VY *= -1;
+			ClampScreen ();
+		}
+	}
+	public void OnMouseDown(){
+		DestroyObj ();
+	}
+}
